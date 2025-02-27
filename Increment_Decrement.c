3caf667b1fa36d681968e76256287500273e195a
utkrrssh <utkarsh23399@gmail.com>
2025-02-27 19:10:38 +0530

    chl re commit hoja

---
+++ Increment_Decrement.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+    int a = 4;
+    printf("The Value of 'a' post-increment:%d\n", a++);
+    printf("After post-increment the current value of 'a' is :%d\n", a);
+    printf("The Value of 'a' pre-increment:%d\n", ++a);
+    printf("After pre-increment the value of 'a' now is :%d\n", a);
+
+return 0;
+}
